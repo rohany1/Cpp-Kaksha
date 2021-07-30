60fe0852a846ccc8fb2601d0a2aced05d92a6de6
Rohan <rohany.hackerank1@gmail.com>
2021-07-30 13:25:49 +0530

    updated kadane's algorithm

--- Basics and Array/kadanesAlgorithm.cpp
+++ Basics and Array/kadanesAlgorithm.cpp
@@ -10,15 +10,23 @@ int main(){
         cin>>arr[i];
     }
 
-    int currSum=0;
-    int maxSum=INT_MIN;
-    for(int i=0;i<n;i++){
-        currSum=currSum+arr[i];
-        if(currSum<0){
-            currSum=0;
-        }
-        maxSum=max(currSum,maxSum);
+    // int currSum=0;
+    // int maxSum=INT_MIN;
+    // for(int i=0;i<n;i++){
+    //     currSum=currSum+arr[i];
+    //     if(currSum<0){
+    //         currSum=0;
+    //     }
+    //     maxSum=max(currSum,maxSum);
+    // }
+    // cout<<maxSum<<" "<<endl;
+    
+    int tillNow=arr[0];
+    int curr=arr[0];
+    for(int i=1;i<n;i++){
+        curr=max(arr[i],arr[i]+curr);
+        tillNow=max(tillNow,curr);
     }
-    cout<<maxSum<<" "<<endl;
+    cout<<tillNow<<endl;
 
 }
